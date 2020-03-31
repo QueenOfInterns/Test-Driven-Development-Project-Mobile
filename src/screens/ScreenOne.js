9f948d2f7f93d71c1fbf15a775791ed230fbea9a
Sarah Sadiq <jisakpj@macx-00177.jbhunt.com>
2020-03-31 17:07:32 -0500

    fixed errors

--- src/screens/ScreenOne.js
+++ src/screens/ScreenOne.js
@@ -1,12 +1,13 @@
-import React, { Component } from 'react';
-import { View, Text } from 'react-native';
+/* eslint-disable react-native/no-inline-styles */
+import React, {Component} from 'react';
+import {View, Text} from 'react-native';
 
 export class ScreenOne extends Component {
-    render() {
-        return (
-            <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center'}}>
-                <Text>Screen One</Text>
-            </View>
-        );
-    }
+  render() {
+    return (
+      <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
+        <Text>Screen One</Text>
+      </View>
+    );
+  }
 }
