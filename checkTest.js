14e985ea281e3998e9778e892f9d20d81f737df9
Kira Threlfall <jisakt4@MACX-00112.local>
2020-04-07 12:44:18 -0500

    added console warning if a non existent file is not chosen, only reads after file was modified, created output folder

--- checkTest.js
+++ checkTest.js
@@ -1,5 +1,18 @@
 const fs = require('fs');
-
-let rawdata = fs.readFileSync('output.json');
-let output = JSON.parse(rawdata);
-console.log(output.testResults[0].status === 'failed' ? 'wrong' : 'right');
+var screen = process.argv[2];
+if (screen < 5 && screen > 0) {
+  const {exec} = require('child_process');
+  exec('jest --json --outputFile=./output/output' + screen + '.json', err => {
+    if (err) {
+      return;
+    }
+  });
+  var watcher = fs.watch('./output/output' + screen + '.json', () => {
+    let data = fs.readFileSync('./output/output' + screen + '.json');
+    let output = JSON.parse(data);
+    console.log(output.testResults[0].status);
+    watcher.close();
+  });
+} else {
+  console.log('\x1b[1m\x1b[33m', 'Choose a screen from 1 to 4 to test');
+}
