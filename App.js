0a44732214a958cb9787fa1edf26692de70e0d8c
Kira Threlfall <jisakt4@MACX-00112.local>
2020-04-08 13:13:42 -0500

    logic to lock tab when previous tab's tests have not passed

--- App.js
+++ App.js
@@ -5,12 +5,30 @@ import {ScreenThree} from './src/screens/ScreenThree';
 import {ScreenFour} from './src/screens/ScreenFour';
 import {createAppContainer} from 'react-navigation';
 import {createBottomTabNavigator} from 'react-navigation-tabs';
+import * as data1 from './output/output1.json';
+import * as data2 from './output/output2.json';
+import * as data3 from './output/output3.json';
+import * as data4 from './output/output4.json';
 
 function screenPass(screen) {
-  const fs = require('react-native-fs');
-  let data = fs.readFileSync('./output/output' + screen + '.json');
-  let output = JSON.parse(data);
-  return output.testResults[0].status === 'failed';
+  var data;
+  switch (screen) {
+    case 1:
+      data = data1;
+      break;
+    case 2:
+      data = data2;
+      break;
+    case 3:
+      data = data3;
+      break;
+    case 4:
+      data = data4;
+      break;
+    default:
+      data = data1;
+  }
+  return !(data.testResults[0].status === 'failed');
 }
 
 const TabNavigator = createBottomTabNavigator(
@@ -32,8 +50,9 @@ const TabNavigator = createBottomTabNavigator(
     defaultNavigationOptions: ({navigation}) => ({
       tabBarOnPress: ({navigation, defaultHandler}) => {
         if (
-          navigation.state.routeName === 'Second' ||
-          navigation.state.routeName === 'Third'
+          (navigation.state.routeName === 'Second' && !screenPass(1)) ||
+          (navigation.state.routeName === 'Third' && !screenPass(2)) ||
+          (navigation.state.routeName === 'Fourth' && !screenPass(3))
         ) {
           return null;
         }
