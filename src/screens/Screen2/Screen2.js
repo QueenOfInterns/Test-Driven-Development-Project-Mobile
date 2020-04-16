71ab3948a83183a351afcb0a0c5b0700287ec0d4
Sarah Sadiq <jisakpj@macx-00177.attlocal.net>
2020-04-16 14:38:36 -0500

    screen 2

--- src/screens/Screen2/Screen2.js
+++ src/screens/Screen2/Screen2.js
@@ -1,11 +1,44 @@
+/* eslint-disable react-native/no-inline-styles */
+/* eslint-disable no-unused-vars */
+/* eslint-disable no-undef */
 import React, {Component} from 'react';
-import {View, Text} from 'react-native';
+import {View, Text, FlatList} from 'react-native';
 
 export class Screen2 extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      users: [
+        {name: 'Spongebob', id: '1'},
+        {name: 'Patrick', id: '2'},
+        {name: 'Sandy', id: '3'},
+        {name: 'Squidward', id: '4'},
+        {name: 'Mr. Krabs', id: '5'},
+        {name: 'Gary', id: '6'},
+      ],
+    };
+  }
+
   render() {
     return (
-      <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
-        <Text>Screen Two</Text>
+      <View>
+        <FlatList
+          keyExtractor={item => item.id}
+          data={this.state.users}
+          renderItem={({item}) => (
+            <View>
+              <Text
+                style={{
+                  flex: 1,
+                  justifyContent: 'center',
+                  alignItems: 'center',
+                }}>
+                {item.name}
+              </Text>
+            </View>
+          )}
+        />
       </View>
     );
   }
