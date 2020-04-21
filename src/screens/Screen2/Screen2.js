ab01b50c28d3b8eb589e9bb358f864e8a9a88b2d
Sarah Sadiq <jisakpj@macx-00177.attlocal.net>
2020-04-21 16:06:14 -0500

    Added some prop data to the Screen 2

--- src/screens/Screen2/Screen2.js
+++ src/screens/Screen2/Screen2.js
@@ -8,18 +8,25 @@ export class Screen2 extends Component {
   constructor(props) {
     super(props);
 
-    this.state = {
-      users: [
-        {name: 'Spongebob', phoneNumber: '479-387-5534'},
-        {name: 'Patrick', phoneNumber: '479-256-5344'},
-        {name: 'Sandy', phoneNumber: '367-899-7070'},
-        {name: 'Squidward', phoneNumber: '367-345-9999'},
-        {name: 'Mr. Krabs', phoneNumber: '504-214-3322'},
-        {name: 'Gary', phoneNumber: '601-405-9977'},
-      ],
-    };
+    if (props.data) {
+      this.state = {
+        users: [
+          {name: 'Spongebob', phoneNumber: '479-387-5534'},
+          {name: 'Patrick', phoneNumber: '479-256-5344'},
+          {name: 'Sandy', phoneNumber: '367-899-7070'},
+          {name: 'Squidward', phoneNumber: '367-345-9999'},
+          {name: 'Mr. Krabs', phoneNumber: '504-214-3322'},
+          {name: 'Gary', phoneNumber: '601-405-9977'},
+        ],
+        error: false,
+      };
+    } else {
+      this.state = {
+        users: [],
+        error: true,
+      };
+    }
   }
-
   renderInfo = ({item}) => {
     return (
       <View
