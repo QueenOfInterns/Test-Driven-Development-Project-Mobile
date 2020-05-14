3030a580c229cd60d464f0abba4dd0ca0bd0da31
Elijah Helf <elijah.helf@jbhunt.com>
2020-05-14 11:59:05 -0500

    added screen 3 testing

---
+++ src/components/myFlatlist/myFlatlist.test.js
@@ -0,0 +1,30 @@
+import MyFlatlist from "./myFlatlist";
+
+describe('MyFlatlist', () => {
+    describe('Custom Flatlist', () => {
+      it('can render with the given data', () => {
+        MyFlatlist.propTypes = {};
+        const renderer = new ShallowRenderer();
+        renderer.render(<MyFlatlist {...props} />);
+        expect(renderer.getRenderOutput()).toMatchSnapshot();
+      });
+  
+      it('does not crash with no data', () => {
+        const props = {
+          data: [],
+        };
+  
+        const renderer = new ShallowRenderer();
+        renderer.render(<MyFlatlist {...props} />);
+        expect(renderer.getRenderOutput()).toMatchSnapshot();
+      });
+      it('displays error when there is an error', () => {
+        const props = {
+          error: true,
+        };
+        const renderer = new ShallowRenderer();
+        renderer.render(<MyFlatlist {...props} />);
+        expect(renderer.getRenderOutput()).toMatchSnapshot();
+      });
+    });
+  });
